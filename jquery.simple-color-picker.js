c701cd1ec0fba5fd3dd81d159902566fa068d540
Klemens Mantzos <klemens@fetzig.at>
2011-09-09 16:06:32 +0200

    major bugfixes, updates and imporvements

--- jquery.simple-color-picker.js
+++ jquery.simple-color-picker.js
@@ -1,56 +1,113 @@
 ﻿
 $.fn.simpleColorPicker = function(options) {
     var defaults = {
-        colorsPerLine: 8,
-        colors: ['#000000', '#444444', '#666666', '#999999', '#cccccc', '#eeeeee', '#f3f3f3', '#ffffff',
-                 '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9900ff', '#ff00ff',
-                 '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#cfe2f3', '#d9d2e9', '#ead1dc',
-                 '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#9fc5e8', '#b4a7d6', '#d5a6bd',
-                 '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6fa8dc', '#8e7cc3', '#c27ba0',
-                 '#cc0000', '#e69138', '#f1c232', '#6aa84f', '#45818e', '#3d85c6', '#674ea7', '#a64d79',
-                 '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#0b5394', '#351c75', '#741b47',
-                 '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#073763', '#20124d', '#4C1130'],
+        colorsPerLine: 9,
+        colors: [ '#000000', '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9900ff', '#ff00ff',
+                  '#444444', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#cfe2f3', '#d9d2e9', '#ead1dc',
+                  '#666666', '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#9fc5e8', '#b4a7d6', '#d5a6bd',
+                  '#999999', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6fa8dc', '#8e7cc3', '#c27ba0',
+                  '#cccccc', '#cc0000', '#e69138', '#f1c232', '#6aa84f', '#45818e', '#3d85c6', '#674ea7', '#a64d79',
+                  '#eeeeee', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#0b5394', '#351c75', '#741b47',
+                  '#f3f3f3', '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#073763', '#20124d', '#4C1130'],
         showEffect: '',
         hideEffect: '',
-        onChangeColor: false
+        onChangeColor: false,
+        ieHackListItem: '<li style="float: none; clear: both; overflow: hidden; background-color: #fff; display: block; height: 1px; line-height: 1px; font-size: 1px; margin-bottom: -2px;"></li>',
+        templates: {
+            colorPicker: '<div id="${prefix}color-picker" class="color-picker" style="position: absolute; left: 0px; top: 0px;"><ul>{{html listItems}}</ul><div style="clear: both;"></div></div>',
+            listItem: '<li id="${prefix}color-${idx}" class="color-box" style="${breakline}background-color: ${color}" title="${color}"></li>'
+        }
     };
 
     var opts = $.extend(defaults, options);
 
     return this.each(function() {
-        var txt = $(this);
-
-        var colorsMarkup = '';
-
-        var prefix = String(txt.attr('id')).replace(/-/g, '') + '_';
-
-        for(var i = 0; i < opts.colors.length; i++){
-            var item = opts.colors[i];
-
-            var breakLine = '';
-            if (i % opts.colorsPerLine == 0)
-                breakLine = 'clear: both; ';
-
-            if (i > 0 && breakLine && $.browser && $.browser.msie && $.browser.version <= 7) {
-                breakLine = '';
-                colorsMarkup += '<li style="float: none; clear: both; overflow: hidden; background-color: #fff; display: block; height: 1px; line-height: 1px; font-size: 1px; margin-bottom: -2px;"></li>';
+        var elem = $(this),
+            colorsMarkup = '',
+            prefix = String(elem.attr('id')).replace(/-/g, '') + '_';
+        
+        var colorToHSL = function (color) {
+            var rgb, min, max, delta, h, s, l;
+            if (color.length == 7) {
+                rgb = [parseInt(color.substring(1, 3), 16) / 255,
+                        parseInt(color.substring(3, 5), 16) / 255,
+                        parseInt(color.substring(5, 7), 16) / 255];
+            } else if (color.length == 4) {
+                rgb = [parseInt(color.substring(1, 2), 16) / 15,
+                        parseInt(color.substring(2, 3), 16) / 15,
+                        parseInt(color.substring(3, 4), 16) / 15];
             }
-
-            colorsMarkup += '<li id="' + prefix + 'color-' + i + '" class="color-box" style="' + breakLine + 'background-color: ' + item + '" title="' + item + '"></li>';
+            
+            var r = rgb[0], 
+                g = rgb[1], 
+                b = rgb[2];
+            min = Math.min(r, Math.min(g, b));
+            max = Math.max(r, Math.max(g, b));
+            delta = max - min;
+            l = (min + max) / 2;
+            s = 0;
+            if (l > 0 && l < 1) {
+                s = delta / (l < 0.5 ? (2 * l) : (2 - 2 * l));
+            }
+            h = 0;
+            if (delta > 0) {
+                if (max == r && max != g) {
+                    h += (g - b) / delta;
+                }
+                if (max == g && max != b) {
+                    h += (2 + (b - r) / delta);
+                }
+                if (max == b && max != r) {
+                    h += (4 + (r - g) / delta);
+                }
+                h /= 6;
+            }
+            return [h, s, l];
+        };
+        
+        for (var i = 0; i < opts.colors.length; i++) {
+            var color = opts.colors[i],
+                breakline = '';
+            
+            if (i % opts.colorsPerLine == 0) {
+                breakline = 'clear: both; ';
+            }
+            
+            if (i > 0 && breakline && $.browser && $.browser.msie && $.browser.version <= 7) {
+                breakline = '';
+                colorsMarkup += opts.ieHackListItem;
+            }
+            
+            // NOTE ugly hack...sorry
+            colorsMarkup += $("<div />").append($.tmpl(opts.templates.listItem, {
+                "prefix": prefix,
+                "idx": i,
+                "breakline": breakline,
+                "color": color
+            })).html();
         }
-
-        var box = $('<div id="' + prefix + 'color-picker" class="color-picker" style="position: absolute; left: 0px; top: 0px;"><ul>' + colorsMarkup + '</ul><div style="clear: both;"></div></div>');
+        
+        var box = $.tmpl(opts.templates.colorPicker, {
+            "prefix": prefix,
+            "listItems": colorsMarkup
+        });
+        
         $('body').append(box);
         box.hide();
 
         box.find('li.color-box').click(function() {
-            if (txt.is('input')) {
-              txt.val(opts.colors[this.id.substr(this.id.indexOf('-') + 1)]);
-              txt.blur();
+            var selected_color = opts.colors[this.id.substr(this.id.indexOf('-') + 1)];
+            if (elem.is('input')) {
+              elem.val(selected_color);
+              elem.blur();
             }
             if ($.isFunction(defaults.onChangeColor)) {
-              defaults.onChangeColor.call(txt, opts.colors[this.id.substr(this.id.indexOf('-') + 1)]);
+              defaults.onChangeColor.call(elem, selected_color);
             }
+            elem.css({
+                "background-color": selected_color,
+                "color": colorToHSL(selected_color)[2] > 0.5 ? '#000' : '#fff'
+            });
             hideBox(box);
         });
 
@@ -63,22 +120,22 @@ $.fn.simpleColorPicker = function(options) {
         });
 
         var positionAndShowBox = function(box) {
-          var pos = txt.offset();
-          var left = pos.left + txt.outerWidth() - box.outerWidth();
+          var pos = elem.offset();
+          var left = pos.left + elem.outerWidth() - box.outerWidth();
           if (left < pos.left) left = pos.left;
-          box.css({ left: left, top: (pos.top + txt.outerHeight()) });
+          box.css({ left: left, top: (pos.top + elem.outerHeight()) });
           showBox(box);
         };
 
-        txt.click(function(event) {
+        elem.click(function(event) {
           event.stopPropagation();
-          if (!txt.is('input')) {
+          if (!elem.is('input')) {
             // element is not an input so probably a link or div which requires the color box to be shown
             positionAndShowBox(box);
           }
         });
 
-        txt.focus(function() {
+        elem.focus(function() {
           positionAndShowBox(box);
         });
 
@@ -92,12 +149,13 @@ $.fn.simpleColorPicker = function(options) {
         }
 
         function showBox(box) {
-            if (opts.showEffect == 'fade')
+            if (opts.showEffect == 'fade') {
                 box.fadeIn();
-            else if (opts.showEffect == 'slide')
+            } else if (opts.showEffect == 'slide') {
                 box.slideDown();
-            else
+            } else {
                 box.show();
+            }
         }
     });
 };
