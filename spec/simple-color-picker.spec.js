07a6009a9dfd5bf70a4175255b9117cc876b8561
Rachel Carvalho <rachel.carvalho@gmail.com>
2013-11-25 21:48:03 -0500

    Adding more tests

--- spec/simple-color-picker.spec.js
+++ spec/simple-color-picker.spec.js
@@ -41,6 +41,14 @@ describe('Simple color picker', function() {
 		it('of which 8 have clear:both (according to colorsPerLine default value)', function() {
 			expect(get_box().find('li.color-box[style*="clear: both"]').length).toBe(8);
 		});
+
+		it('the first li.color-box has background-color #000000', function() {
+			expect(get_box().find('li.color-box:first').attr('style').indexOf('background-color: #000000')).toBeGreaterThan(-1);
+		});
+
+		it('and title #000000', function() {
+			expect(get_box().find('li.color-box:first').attr('title')).toBe('#000000');
+		});
 	});
 
 	describe('options', function() {
@@ -69,8 +77,70 @@ describe('Simple color picker', function() {
 			it('one of which has clear:both', function() {
 				expect(get_box().find('li.color-box[style*="clear: both"]').length).toBe(1);
 			});
-
 		});
 	});
 
+	describe('behavior', function() {
+		var box = null;
+
+		beforeEach(function() {
+			txt.simpleColorPicker();
+			box = get_box();
+		});
+
+		describe('focusing input', function() {
+			it('positions color picker aligned to input right', function() {
+				txt.focus();
+				expect(box.offset().left + box.outerWidth()).toBe(txt.offset().left + txt.outerWidth());
+			});
+
+			it('or to left, if picker is wider than input', function() {
+				txt.width(10).focus();
+				expect(box.offset().left).toBe(txt.offset().left);
+			});
+
+			it('shows color picker', function() {
+				expect(box.is(':hidden')).toBeTruthy();
+				txt.focus();
+				expect(box.is(':hidden')).not.toBeTruthy();
+			});
+		});
+
+		describe('after picker is open', function() {
+			beforeEach(function() {
+				txt.focus();
+			});
+
+			it('clicking outside closes it', function() {
+				expect(box.is(':hidden')).not.toBeTruthy();
+				$('body').click();
+				expect(box.is(':hidden')).toBeTruthy();
+			});
+
+			it('clicking another element closes it', function() {
+				expect(box.is(':hidden')).not.toBeTruthy();
+				var btn = $(document.createElement('button'));
+				$('body').append(btn);
+				btn.click();
+				expect(box.is(':hidden')).toBeTruthy();
+				btn.remove();
+			});
+
+			describe('clicking a color', function() {
+				var color_li = null;
+				beforeEach(function() {
+					color_li = box.find('li.color-box:first');
+					color_li.click();
+				});
+
+				it('fills the input with the right color code', function() {
+					expect(txt.val()).toBe(color_li.attr('title'));
+				});
+
+				it('hides the picker', function() {
+					expect(box.is(':hidden')).toBeTruthy();
+				});
+			});
+		});
+	});
 });
